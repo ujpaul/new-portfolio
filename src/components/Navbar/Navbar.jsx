77c58bdce0a9335ec7529bc6f6d659ab127d814b
uwimana jean paul <uwimanajeanpaul3@gmail.com>
2023-02-14 17:08:12 +0200

    change logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,13 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.logo} alt='logo' />
+        <a href='#home'>
+          <img
+            src={images.myLogo}
+            style={{ height: 40, cursor: "pointer" }}
+            alt='logo'
+          />
+        </a>
       </div>
       <ul className='app__navbar-links'>
         {["home", "about", "work", "skills", "contact"].map((item) => (
